ca7a883fb517c9de3240b147ad2a859683a92156
jinwoo park <swat018@gmail.com>
2022-01-08 21:47:09 +0900

    Initial commit

---
+++ src/test/java/me/swat018/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.swat018.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
